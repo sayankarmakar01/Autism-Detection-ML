d1e65c9908afb6ac2547b8c72e94ad6f0718d3e4
Sayan Karmakar <89528429+sayankarmakar01@users.noreply.github.com>
2025-06-20 14:32:00 +0530

    Update system_connect.php
    Change the PASS

--- WEB/Major/login/include/system_connect.php
+++ WEB/Major/login/include/system_connect.php
@@ -3,6 +3,6 @@ ob_start();
 session_start();
 putenv('TZ=Asia/Calcutta');
 date_default_timezone_set("Asia/Calcutta");
-$mysqli=new mysqli("localhost","thedeltas_usrmajor","}GF5;^7Ex(ja.Dw1","thedeltas_major");
+$mysqli=new mysqli("localhost","root","","major");
 $user_id=$_SESSION['user_id'];
 ?>
